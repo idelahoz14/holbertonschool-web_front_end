9100fd3a920d820d3450a98953e0770a36e4d2e9
idelahoz14 <idelahoz14@cuc.edu.co>
2021-06-22 23:17:28 -0500

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+    alert("Welcome " + fullName);
+}
+
+guillame = function() {welcomeMessage("Guillaume")};
+alex = function() {welcomeMessage("Alex")};
+fred = function() {welcomeMessage("Fred")};
